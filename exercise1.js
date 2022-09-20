816355d3672c62040a85379d4c271d3022931cea
Shunyi Xu <3051778755@qq.com>
2022-09-20 18:30:53 +0800

    Exercise 1.

---
+++ exercise1.js
@@ -0,0 +1,13 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => {
+     const completed = json.filter((todo,index) => {
+        if ( todo.completed == true ){
+            console.log(`${todo.title} - ${todo.completed}`)
+        }    
+     });
+     console.log(completed)
+  })
+  .catch(function(err) { 
+    console.log(err);
+  });
